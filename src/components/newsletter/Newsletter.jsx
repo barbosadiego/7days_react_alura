c30ae08aa2011a93d2cb862fae2cdd7c0c797b3a
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-24 10:48:09 -0300

    add newsletter component

---
+++ src/components/newsletter/Newsletter.jsx
@@ -0,0 +1,12 @@
+import './Newsletter.scss';
+
+const Newsletter = () => {
+  return (
+    <div className="newsletter">
+      <input type="text" placeholder="Insira seu email" />
+      <button>Assinar newsletter</button>
+    </div>
+  );
+};
+
+export default Newsletter;
