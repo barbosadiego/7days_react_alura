87daafc6afa9c4d7380c705385e459b0267744f5
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-25 21:50:55 -0300

    reorder hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,32 @@ import styled from 'styled-components';
 import background from '../assets/imagem-hero.png';
 import Newsletter from './Newsletter';
 
+const Hero = () => {
+  return (
+    <>
+      <StyledHero>
+        <StyledIntro>
+          <div>
+            <p>Sua casa com as</p>
+            <h1>melhores plantas</h1>
+          </div>
+          <p>
+            Encontre aqui uma vasta seleção de plantas para decorar a sua casa e
+            torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu
+            e-mail e assine nossa newsletter para saber das novidades da marca.
+          </p>
+          <Newsletter />
+        </StyledIntro>
+        <div>
+          <img src={background} alt="planta" />
+        </div>
+      </StyledHero>
+    </>
+  );
+};
+
+export default Hero;
+
 const StyledHero = styled.section`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
@@ -24,6 +50,13 @@ const StyledIntro = styled.div`
   grid-template-columns: 1fr;
   gap: 1.5rem;
 
+  & > div {
+    p {
+      font-size: 25px;
+      margin-bottom: 1rem;
+    }
+  }
+
   h1 {
     font-weight: 900;
     font-size: 82px;
@@ -34,29 +67,3 @@ const StyledIntro = styled.div`
     }
   }
 `;
-
-const Hero = () => {
-  return (
-    <>
-      <StyledHero>
-        <StyledIntro>
-          <div>
-            <p>Sua casa com as</p>
-            <h1 className="subtitle">melhores plantas</h1>
-          </div>
-          <p>
-            Encontre aqui uma vasta seleção de plantas para decorar a sua casa e
-            torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu
-            e-mail e assine nossa newsletter para saber das novidades da marca.
-          </p>
-          <Newsletter />
-        </StyledIntro>
-        <div>
-          <img src={background} alt="planta" />
-        </div>
-      </StyledHero>
-    </>
-  );
-};
-
-export default Hero;
