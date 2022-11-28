31a4db22bae0dee583c3bfeb7cbb467a1ab49d8a
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-28 16:29:31 -0300

    create titles styled component

---
+++ src/components/styles/Titles.styled.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-weight: 900;
+  font-size: 82px;
+  line-height: 94px;
+  @media screen and (max-width: 500px) {
+    font-size: 70px;
+    line-height: 75px;
+  }
+`;
+
+export const Subtitle = styled.h2`
+  font-size: 62px;
+  font-weight: 900;
+  line-height: 74px;
+`;
