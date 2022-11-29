84ea2f6e9bf36f7cd75f98a27cc3c17abb32f5a9
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-29 11:43:27 -0300

    create methods for sort data

---
+++ src/helpers/sortItems.js
@@ -0,0 +1,15 @@
+export function sortByName(data) {
+  return data.sort((a, b) => {
+    const nameA = a.name.toLowerCase();
+    const nameB = b.name.toLowerCase();
+
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
+
+    return 0;
+  });
+}
+
+export function sortByPrice(data) {
+  return data.sort((a, b) => a.preco - b.preco);
+}
