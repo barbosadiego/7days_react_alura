7f9f58e2a787813facdf829b22f9ada804ce2b99
diegobarbosa <diego.gutemberg@hotmail.com>
2022-11-30 11:54:59 -0300

    update paddin in button styled component

--- src/components/styles/Button.styled.jsx
+++ src/components/styles/Button.styled.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Button = styled.button`
   background-color: ${({ theme }) => theme.color.yellow};
   color: ${({ theme }) => theme.color.white};
-  padding: 1rem;
+  padding: 0.5rem 1rem;
   font-size: 1rem;
   font-family: 'Montserrat';
   margin-bottom: 1rem;
